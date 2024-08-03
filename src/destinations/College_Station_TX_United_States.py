7533fd92816d59803294cc968887049e6e343abe
Jaydeep Agravat <117134691+JaydeepAgravat@users.noreply.github.com>
2024-08-03 13:24:30 +0000

    Initial commit

---
+++ src/destinations/College_Station_TX_United_States.py
@@ -0,0 +1,21 @@
+import sys
+from pathlib import Path
+from datetime import datetime, timedelta
+
+src_path = Path(__file__).resolve().parent.parent.parent
+sys.path.append(str(src_path))
+from main import scrape
+from src.utils.logger import CustomLogger
+
+if __name__ == "__main__":
+                destination = "College Station, TX, United States"
+                number_of_days = 1
+                unique_dates = [
+                    (datetime.now() + timedelta(days=i)).strftime("%d/%m/%Y")
+                    for i in range(number_of_days)
+                ]
+                log = CustomLogger("College Station, TX, United States")
+                log.info(
+                    f"scraper is running for {destination} : {unique_dates}")
+                scrape(destination, number_of_days)
+                log.info(f"scraper finished for {destination}")
